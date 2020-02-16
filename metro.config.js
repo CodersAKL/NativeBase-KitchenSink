612601b65a4cfe454287c5abbca4b3efb4b2c256
Vytenis Kuciauskas <projektas@gmail.com>
2020-02-16 15:38:28 +0200

    Upgrade

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+const {
+    applyConfigForLinkedDependencies,
+} = require("@carimus/metro-symlinked-deps");
+
+module.exports = applyConfigForLinkedDependencies(
+    {
+        transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+        },
+    },
+    {
+        projectRoot: __dirname,
+        blacklistLinkedModules: ["react-native"],
+    },
+);
